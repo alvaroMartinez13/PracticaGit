6188f5555bb814a35d3884c19e568167b4074a23
MarioFelipe19 <felipeparra257@hotmail.com>
2024-07-11 21:36:49 -0500

    feat: :sparkles: funciones de suma y multiplicacion
    se empezo a desarrollar las funciones de suma y multiplicacion
    
    By @ MarioParra

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+from main import *
+
+def suma():
+    num1 = int(input("ingrese numero 1: "))
+    num2 = int(input("ingrese numero 1: "))
+    num3 = num1 + num2
+    return num3
+
+def multipliccion():
+    num1 = int(input("ingrese numero 1: "))
+    num2 = int(input("ingrese numero 1: "))
+    num3 = num1 * num2
+    return num3
